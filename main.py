fa5a8fc37a0644751d4c99aaad4ef3395debe196
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-06-22 21:54:21 +0900

    Load the bot from command_manager

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os
+import sys
+
+import discord
+from dotenv import load_dotenv
+
+from cmd import command_manager
+
+
+# Read .env
+load_dotenv()
+
+# Check discord token
+DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN")
+if DISCORD_TOKEN is None or DISCORD_TOKEN == "":
+    print("Failed to load discord token!\nPlease set discord token to your .env file.")
+    sys.exit(1)
+
+# Init discord components
+intents: discord.Intents = discord.Intents().all()
+client: discord.Client = discord.Client(intents=intents)
+
+
 def main():
-    print("Hello")
+    global DISCORD_TOKEN
+
+    command_manager.bot.run(DISCORD_TOKEN)
 
 
 if __name__ == '__main__':
