9ed78dc73ca3d926b40f24f97da2242c95436c24
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-06-24 19:32:40 +0900

    Rewrite join.py

--- cmd/join.py
+++ cmd/join.py
@@ -1,10 +1,14 @@
+import asyncio
+
 import discord.ext.commands
 
 
 async def join_exec(ctx: discord.ext.commands.Context):
-    if not ctx.message.author.voice:
-        await ctx.send("ボイスチャンネルに接続してからコマンドを実行してください")
-        return
-    else:
-        channel = ctx.message.author.voice.channel
-    await channel.connect()
+    try:
+        vc = ctx.author.voice.channel
+
+        await vc.connect(timeout=10, reconnect=False)
+    except discord.ClientException:
+        await ctx.reply("VCに接続してから再度コマンドを実行してください。")
+    except asyncio.TimeoutError:
+        await ctx.reply("VCへの接続がタイムアウトしました。")
