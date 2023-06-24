dc5f7ef18afc02a4c02302fc2ff3d057d8786a54
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-06-24 16:30:52 +0900

    Delete a class

--- main.py
+++ main.py
@@ -21,42 +21,6 @@ if DISCORD_TOKEN is None or DISCORD_TOKEN == "":
 intents: discord.Intents = discord.Intents().all()
 client: discord.Client = discord.Client(intents=intents)
 
-ytdl_opt = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'
-}
-
-ffmpeg_opt = {
-    'options': '-vn -volume=-6dB'
-}
-
-ytdl = yt_dlp.YoutubeDL(ytdl_opt)
-
-
-class DataSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = ""
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
-
 
 def main():
     global DISCORD_TOKEN
