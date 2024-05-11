42c06d4ff631f1c8743bb36c6aab4c7a26b9225f
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-05-11 23:58:01 +0900

    build: Replace gradle project

--- cmd/ping.py
+++
@@ -1,6 +0,0 @@
-from discord.ext.commands import Context
-
-
-async def ping_exec(ctx: Context):
-    await ctx.reply("Pong!")
-
--- cmd/quit.py
+++
@@ -1,9 +0,0 @@
-import discord.ext.commands
-
-
-async def quit_exec(ctx: discord.ext.commands.Context):
-    voice_client = ctx.channel.guild.voice_client
-    if voice_client.client.is_ready():
-        await voice_client.disconnect(force=True)
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
